9a748d441e9e9b98a3c89c9a8265b3aeb40176cc
Olga <bansheerelease@gmail.com>
2012-03-10 10:13:48 +0300

    first commit

---
+++ app/models/admin.rb
@@ -0,0 +1,10 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable  , :registerable,
+  # :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end

959f37e612344cedf75a2572111d040650613ed9
bansheerelease <bansheerelease@gmail.com>
2012-03-11 12:54:36 +0300

    Stylesheets work

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -16,7 +16,7 @@ class UserInfosController < ApplicationController
   # GET /user_infos/1.json
   def show
 
-    @user_info = UserInfo.find_by_id(current_user.id)
+    @user_info = UserInfo.find(current_user.id)
     if @user_info.nil?
       current_user.user_info.build()
       @user_info = UserInfo.find_by_id(current_user.id)

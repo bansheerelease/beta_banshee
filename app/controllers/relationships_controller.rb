0df245e32dfb6fb109753c75cc19143bbed40080
bansheerelease <bansheerelease@gmail.com>
2012-03-13 21:20:43 +0300

    Fully completed user blogs. Added followers/following relations, showing post from all users you folow. Added some tests

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,6 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
+    @tag_name = ('follow_form'+@user.id.to_s+'f')
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -13,9 +14,10 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
+    @tag_name = ('follow_form'+@user.id.to_s+'u')
     respond_to do |format|
       format.html { redirect_to @user }
-      format.js
+      format.js {}
     end
   end
 end

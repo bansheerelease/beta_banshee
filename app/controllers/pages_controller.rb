d88fdcd88254302e49d79f4285283dd1da17bc0d
bansheerelease <bansheerelease@gmail.com>
2012-03-25 12:42:04 +0300

    Finished microposts + search(I hope)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -60,21 +60,29 @@ class PagesController < ApplicationController
     found = false;
     #посмотреть, если есть пробелы, то юзаем :any, иначе :all
     if (!params[:user].nil?)
-      @search_res_users = User.search(params[:search], :match_mode => :any).page(params[:current_page]).per(5)
+      if (params[:search].empty?)
+        @search_res_users = Kaminari.paginate_array(User.all).page(params[:current_page]).per(10)
+      else
+        @search_res_users = User.search(params[:search], :match_mode => :any).page(params[:current_page]).per(10)
+      end
       if (!@search_res_users.empty?)
         found = true
       end
     end
     if (!params[:profiles].nil?)
-      @user_infos = UserInfo.search(params[:search], :match_mode => :any).page(params[:current_page]).per(5)
-      if (!@user_infos.empty?)
-        found = true
+      if (!params[:search].empty?)
+        @user_infos = UserInfo.search(params[:search], :match_mode => :any).page(params[:current_page]).per(10)
+        if (!@user_infos.empty?)
+          found = true
+        end
       end
     end
     if (!params[:microposts].nil?)
-      @microposts = Micropost.search(params[:search], :match_mode => :any).page(params[:current_page]).per(5)
-      if (!@microposts.empty?)
-        found = true
+      if (!params[:search].empty?)
+        @microposts = Micropost.search(params[:search], :match_mode => :any).page(params[:current_page]).per(5)
+        if (!@microposts.empty?)
+          found = true
+        end
       end
     end
     if (!found)
